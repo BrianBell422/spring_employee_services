d1c352fb69c4712dbe56d613c2e724344a8ff02e
BrianBell422 <brianbell.uk@gmail.com>
2021-04-06 13:04:51 +0100

    Added onetomany and manytomany relationships

--- src/main/java/com/example/EmployeeServices/models/Employee.java
+++ src/main/java/com/example/EmployeeServices/models/Employee.java
@@ -1,6 +1,11 @@
 package com.example.EmployeeServices.models;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import org.hibernate.annotations.Cascade;
+
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Table(name = "employees")
@@ -24,15 +29,39 @@ public class Employee {
     @Column(name = "email")
     private String email;
 
+    @ManyToOne
+    @JoinColumn(name = "department_id", nullable = false)
+    @JsonIgnoreProperties({"employees"})
+    private Department department;
+
+    @ManyToMany
+    @JsonIgnoreProperties({"employees"})
+    @Cascade(org.hibernate.annotations.CascadeType.SAVE_UPDATE)
+    @JoinTable(
+            name = "employees_projects",
+            joinColumns = { @JoinColumn(
+                    name = "employee_id",
+                    nullable = false,
+                    updatable = false)
+            },
+            inverseJoinColumns = { @JoinColumn(
+                    name = "project_id",
+                    nullable = false,
+                    updatable = false)
+            })
+    private List<Project> projects;
+
     public Employee() {
 
     }
 
-    public Employee(String name, int age, int employeeNumber, String email) {
+    public Employee(String name, int age, int employeeNumber, String email, Department department) {
         this.name = name;
         this.age = age;
         this.employeeNumber = employeeNumber;
         this.email = email;
+        this.department = department;
+        this.projects = new ArrayList<>();
     }
 
     public Long getId() {
@@ -73,5 +102,25 @@ public class Employee {
 
     public void setEmail(String email) {
         this.email = email;
+    }
+
+    public Department getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(Department department) {
+        this.department = department;
+    }
+
+    public List<Project> getProjects() {
+        return projects;
+    }
+
+    public void setProjects(List<Project> projects) {
+        this.projects = projects;
+    }
+
+    public void addProject(Project project) {
+        this.projects.add(project);
     }
 }
--- src/test/java/com/example/EmployeeServices/EmployeeServicesApplicationTests.java
+++ src/test/java/com/example/EmployeeServices/EmployeeServicesApplicationTests.java
@@ -1,7 +1,11 @@
 package com.example.EmployeeServices;
 
+import com.example.EmployeeServices.models.Department;
 import com.example.EmployeeServices.models.Employee;
+import com.example.EmployeeServices.models.Project;
+import com.example.EmployeeServices.repositories.DepartmentRepository;
 import com.example.EmployeeServices.repositories.EmployeeRepository;
+import com.example.EmployeeServices.repositories.ProjectRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -10,16 +14,42 @@ import org.springframework.boot.test.context.SpringBootTest;
 class EmployeeServicesApplicationTests {
 
 	@Autowired
+	ProjectRepository projectRepository;
+
+	@Autowired
 	EmployeeRepository employeeRepository;
+
+	@Autowired
+	DepartmentRepository departmentRepository;
 
 	@Test
 	void contextLoads() {
 	}
 
 	@Test
-	public void createEmployee() {
-		Employee brian = new Employee("Brian", 33, 101, "brian@brian.com");
-		employeeRepository.save(brian);
+	public void createEmployeeAndDepartment() {
+		Department department = new Department("Technology");
+		departmentRepository.save(department);
+
+		Employee employee = new Employee("Brian", 33, 101, "brian@brian.com", department);
+		employeeRepository.save(employee);
 	}
+
+	@Test
+	public void addEmployeesAndProjects() {
+		Department department = new Department("Technology");
+		departmentRepository.save(department);
+
+		Employee employee = new Employee("Brian", 33, 101, "brian@brian.com", department);
+		employeeRepository.save(employee);
+
+		Project project = new Project("Big Project", 7);
+		projectRepository.save(project);
+
+		project.addEmployee(employee);
+		projectRepository.save(project);
+	}
+
+
 
 }
